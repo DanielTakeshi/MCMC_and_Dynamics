e8ce7f4264acd892878578df988a7d6ec75bde7e
DanielTakeshi <takeshidanny@gmail.com>
2017-12-14 14:51:00 -0800

    Fixed 3 tuning scripts. Now run.

--- bnn/tianqi_chen/experiments/tune_sghmc.sh
+++ bnn/tianqi_chen/experiments/tune_sghmc.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 for e in 0.001 0.01 0.1 0.5; do
-    for w in 0.0 0.00001 0.0001, 0.001, 0.01; do
-        for (( i=1 ; i <= 5 ; i++ )); do
-            python mnist-sghmc.py --algo 'sghmc' --eta $e --wd $w --seed $i 2> logs/sghmc-tune/eta-$e-wd-$w-seed-$i
-        done
+    for (( i=1 ; i <= 5 ; i++ )); do
+        python mnist-sghmc.py --algo 'sghmc' --eta $e --seed $i 2> logs/sghmc-tune/eta-$e-seed-$i
     done
 done
--- bnn/tianqi_chen/experiments/tune_sgld.sh
+++ bnn/tianqi_chen/experiments/tune_sgld.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 for e in 0.001 0.01 0.1 0.5; do
-    for w in 0.0 0.00001 0.0001, 0.001, 0.01; do
-        for (( i=1 ; i <= 5 ; i++ )); do
-            python mnist-sghmc.py --algo 'sgld' --eta $e --wd $w --seed $i 2> logs/sgld-tune/eta-$e-wd-$w-seed-$i
-        done
+    for (( i=1 ; i <= 5 ; i++ )); do
+        python mnist-sghmc.py --algo 'sgld' --eta $e --seed $i 2> logs/sgld-tune/eta-$e-seed-$i
     done
 done
