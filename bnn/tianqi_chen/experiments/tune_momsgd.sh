0bd19404ea21f918fe3374f9fb5808a72b45740d
DanielTakeshi <takeshidanny@gmail.com>
2017-12-15 21:09:16 -0800

    Whoops fixed typo in bash scripts

--- bnn/tianqi_chen/experiments/tune_momsgd.sh
+++ bnn/tianqi_chen/experiments/tune_momsgd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for e in 0.001 0.01 0.1 0.5; do
-    for w in 0.0 0.00001 0.0001, 0.001, 0.01; do
+    for w in 0.0 0.00001 0.0001 0.001 0.01; do
         for (( i=1 ; i <= 5 ; i++ )); do
             python mnist-sghmc.py --algo 'momsgd' --eta $e --wd $w --seed $i 2> logs/momsgd-tune/eta-$e-wd-$w-seed-$i
         done
