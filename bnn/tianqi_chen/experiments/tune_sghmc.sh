dc3a4d40033ab5ab8af855dd890f65bf1108b0df
DanielTakeshi <takeshidanny@gmail.com>
2017-12-18 08:23:30 -0800

    Whoops, didn't add wd arg in scripts for sghmc, sgld

--- bnn/tianqi_chen/experiments/tune_sghmc.sh
+++ bnn/tianqi_chen/experiments/tune_sghmc.sh
@@ -2,6 +2,6 @@
 
 for e in 0.001 0.01 0.1 0.5; do
     for (( i=1 ; i <= 5 ; i++ )); do
-        python mnist-sghmc.py --algo 'sghmc' --eta $e --seed $i 2> logs/sghmc-tune/eta-$e-seed-$i
+        python mnist-sghmc.py --algo 'sghmc' --eta $e --wd 0.00002 --seed $i 2> logs/sghmc-tune/eta-$e-seed-$i
     done
 done
--- bnn/tianqi_chen/experiments/tune_sgld.sh
+++ bnn/tianqi_chen/experiments/tune_sgld.sh
@@ -2,6 +2,6 @@
 
 for e in 0.001 0.01 0.1 0.5; do
     for (( i=1 ; i <= 5 ; i++ )); do
-        python mnist-sghmc.py --algo 'sgld' --eta $e --seed $i 2> logs/sgld-tune/eta-$e-seed-$i
+        python mnist-sghmc.py --algo 'sgld' --eta $e --wd 0.00002 --seed $i 2> logs/sgld-tune/eta-$e-seed-$i
     done
 done
